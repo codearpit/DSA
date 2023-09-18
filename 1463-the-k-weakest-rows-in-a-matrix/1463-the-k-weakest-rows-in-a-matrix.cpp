184ea2646f2064dd7641ee7c82f5c41710bc5777
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-19 02:08:49 +0530

    Time: 7 ms (82.22%), Space: 11.2 MB (5.28%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -15,7 +15,7 @@ public:
     }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         priority_queue<pair<int, int>> pq;
-        vector<int> ans(k,-1);
+        vector<int> ans;
 
         for(int i = 0; i < mat.size(); i++){
             int ones = binary(mat[i]);
@@ -24,10 +24,13 @@ public:
                 pq.pop();
             }
         }
-        for(int i = k - 1; i >= 0; i--){
-            ans[i] = pq.top().second;
+        for(int i = 0; i<k; ++i)
+        {
+            ans.push_back(pq.top().second);
             pq.pop();
         }
+        
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
