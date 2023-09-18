6612d08182edfea153f0901280cc333f752f766b
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-19 02:09:52 +0530

    Time: 10 ms (53.98%), Space: 10.8 MB (9.72%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-
-    int binary(vector<int> arr){
-        int hi = arr.size() - 1;
-        int lo = 0;
-        while(hi >= lo){
-            int mid = lo + (hi - lo)/2;
-            if(arr[mid] == 0){
-                hi = mid - 1;
-            }
-            else lo = mid + 1;
-        }
-        return lo;
-    }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        priority_queue<pair<int, int>> pq;
-        vector<int> ans;
-
-        for(int i = 0; i < mat.size(); i++){
-            int ones = binary(mat[i]);
-            pq.push(make_pair(ones, i));
-            if(pq.size()>k){
-                pq.pop();
-            }
-        }
-        for(int i = 0; i<k; ++i)
-        {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        vector<pair<int, int>> rowStrengths;
+        for (int i = 0; i < mat.size(); ++i) {
+            int strength = accumulate(mat[i].begin(), mat[i].end(), 0);
+            rowStrengths.push_back({strength, i});
         }
         
-        reverse(ans.begin(),ans.end());
-        return ans;
+        sort(rowStrengths.begin(), rowStrengths.end());
+        
+        vector<int> result;
+        for (int i = 0; i < k; ++i) {
+            result.push_back(rowStrengths[i].second);
+        }
+        
+        return result;
     }
 };
