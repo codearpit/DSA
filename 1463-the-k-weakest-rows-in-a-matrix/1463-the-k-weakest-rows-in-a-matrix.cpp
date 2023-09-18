619f39c40f53d7ced063d602e5dc097998933278
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-19 02:12:17 +0530

    Time: 10 ms (53.98%), Space: 10.8 MB (9.72%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,19 +1,37 @@
+// Define a custom comparison function to sort rows based on the number of soldiers and row index
+bool compareRows(pair<int, int>& a, pair<int, int>& b) {
+    if (a.first != b.first) {
+        return a.first < b.first;  // Sort by the number of soldiers
+    }
+    return a.second < b.second;    // If soldiers are equal, sort by row index
+}
+
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<pair<int, int>> rowStrengths;
-        for (int i = 0; i < mat.size(); ++i) {
-            int strength = accumulate(mat[i].begin(), mat[i].end(), 0);
-            rowStrengths.push_back({strength, i});
+    int m = mat.size();
+    int n = mat[0].size();
+
+    // Create a vector of pairs (count of soldiers, row index)
+    vector<pair<int, int>> rows;
+
+    for (int i = 0; i < m; ++i) {
+        int soldiers = 0;
+        for (int j = 0; j < n; ++j) {
+            soldiers += mat[i][j];
         }
-        
-        sort(rowStrengths.begin(), rowStrengths.end());
-        
-        vector<int> result;
-        for (int i = 0; i < k; ++i) {
-            result.push_back(rowStrengths[i].second);
-        }
-        
-        return result;
+        rows.push_back({soldiers, i});
+    }
+
+    // Sort the rows using the custom comparison function
+    sort(rows.begin(), rows.end(), compareRows);
+
+    // Extract the indices of the k weakest rows
+    vector<int> result;
+    for (int i = 0; i < k; ++i) {
+        result.push_back(rows[i].second);
+    }
+
+    return result;
     }
 };
