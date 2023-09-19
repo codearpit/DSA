33af90ad3f3f14769ca2efcbfffd0b528b60a6d6
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-20 01:42:48 +0530

    Time: 149 ms (19.92%), Space: 84.2 MB (14.39%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-    
-
-    map<int, bool> arr;
-
-    for (int i = 0; i < nums.size(); i++){
-
-        if(arr[nums.at(i)] == true)
-            return nums.at(i);
-            else arr[nums.at(i)] = true;
-        
+    int findDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.find(num) != seen.end()) {
+                return num;
+            }
+            seen.insert(num);
+        }
+        return -1;  // Just to satisfy the compiler, this should never be reached
     }
-    return 0;
-    }
-    
 };
