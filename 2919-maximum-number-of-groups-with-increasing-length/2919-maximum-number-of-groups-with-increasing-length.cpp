e1a7ebd255ae4b1c56ab28513eaa686942c43f51
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-20 16:48:47 +0530

    Time: 322 ms (8.13%), Space: 183.9 MB (5.05%) - LeetHub

--- 2919-maximum-number-of-groups-with-increasing-length/2919-maximum-number-of-groups-with-increasing-length.cpp
+++ 2919-maximum-number-of-groups-with-increasing-length/2919-maximum-number-of-groups-with-increasing-length.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int maxIncreasingGroups(vector<int>& A) {
-        sort(A.begin(), A.end());
+        int n = A.size();
+        unordered_map<int, int> count;
+        for (int a : A)
+            count[min(a, n)]++;
         long long total = 0, k = 0;
-        for (int a : A) {
-            total += a;
-            if (total >= (k + 1) * (k + 2) / 2)
-                k++;
-        }
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j < count[i]; j++)
+                if ((total += i) >= (k + 1) * (k + 2) / 2)
+                    k++;
         return k;
     }
 };
