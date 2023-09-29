506033d8e948eb6db9f5712ebd8fc33ecfccd97b
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-29 19:40:45 +0530

    Time: 117 ms (76.21%), Space: 96.9 MB (9.17%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
 
-    bool isMonotonic(vector<int> A) {
-        bool inc = true, dec = true;
-        for (int i = 1; i < A.size(); ++i){
-            inc &= A[i - 1] <= A[i];
-            dec &= A[i - 1] >= A[i];
-        }
-        return inc || dec;
-    }
+    // bool isMonotonic(vector<int> A) {
+    //     bool inc = true, dec = true;
+    //     for (int i = 1; i < A.size(); ++i){
+    //         inc &= A[i - 1] <= A[i];
+    //         dec &= A[i - 1] >= A[i];
+    //     }
+    //     return inc || dec;
+    // }
 
-    // bool isMonotonic(vector<int>& arr) {
-    //     int i = 0;
-    //     int n = arr.size();
-    //     while(i<n - 1){
-    //         if(arr[i] == arr[i+1]) i++;
+    bool isMonotonic(vector<int>& arr) {
+        int i = 0;
+        int n = arr.size();
+        while(i<n - 1){
+            if(arr[i] == arr[i+1]) i++;
 
-    //         else if(arr[i] < arr[i+1]){
-    //             while(i < n - 1){
-    //                 if(arr[i] > arr[i+1]) return false;
-    //                 i++;
-    //             }
-    //         }
-    //         else{
-    //          while(i < n - 1){
-    //                 if(arr[i] < arr[i+1]) return false;
-    //                 i++;
-    //             }
-    //         }   
+            else if(arr[i] < arr[i+1]){
+                while(i < n - 1){
+                    if(arr[i] > arr[i+1]) return false;
+                    i++;
+                }
+            }
+            else{
+             while(i < n - 1){
+                    if(arr[i] < arr[i+1]) return false;
+                    i++;
+                }
+            }   
 
         
-    //     }
-    //     return true;
-    // }
+        }
+        return true;
+    }
 };
