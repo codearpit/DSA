3adfd551c85be64cd754b2a1f39adb13cff59173
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-29 19:42:25 +0530

    Time: 83 ms (99.3%), Space: 96.6 MB (38.42%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -17,46 +17,46 @@ public:
     //     return inc || dec;
     // }
 
-    // bool isMonotonic(vector<int>& arr) {
-    //     int i = 0;
-    //     int n = arr.size();
-    //     while(i<n - 1){
-    //         if(arr[i] == arr[i+1]) i++;
+    bool isMonotonic(vector<int>& arr) {
+        int i = 0;
+        int n = arr.size();
+        while(i<n - 1){
+            if(arr[i] == arr[i+1]) i++;
 
-    //         else if(arr[i] < arr[i+1]){
-    //             while(i < n - 1){
-    //                 if(arr[i] > arr[i+1]) return false;
-    //                 i++;
-    //             }
-    //         }
-    //         else{
-    //          while(i < n - 1){
-    //                 if(arr[i] < arr[i+1]) return false;
-    //                 i++;
-    //             }
-    //         }   
+            else if(arr[i] < arr[i+1]){
+                while(i < n - 1){
+                    if(arr[i] > arr[i+1]) return false;
+                    i++;
+                }
+            }
+            else{
+             while(i < n - 1){
+                    if(arr[i] < arr[i+1]) return false;
+                    i++;
+                }
+            }   
 
         
-    //     }
-    //     return true;
-    // }
-    bool isMonotonic(vector<int>& nums) {
-        int size = nums.size();
-        if (size == 1){return true;}
-        int startPos = 1;
-        while (startPos < size){
-            if (nums[startPos - 1] != nums[startPos]){
-                startPos--;
-                break;
-            }
-            startPos++;
-        }
-        if (startPos == size){return true;}
-        int diff = nums[startPos + 1] - nums[startPos];
-        for (startPos; startPos < size - 1; startPos++){
-            if (diff > 0 && nums[startPos + 1] - nums[startPos] < 0) {return false;}
-            else if (diff < 0 && nums[startPos + 1] - nums[startPos] > 0) {return false;}
         }
         return true;
     }
+    // bool isMonotonic(vector<int>& nums) {
+    //     int size = nums.size();
+    //     if (size == 1){return true;}
+    //     int startPos = 1;
+    //     while (startPos < size){
+    //         if (nums[startPos - 1] != nums[startPos]){
+    //             startPos--;
+    //             break;
+    //         }
+    //         startPos++;
+    //     }
+    //     if (startPos == size){return true;}
+    //     int diff = nums[startPos + 1] - nums[startPos];
+    //     for (startPos; startPos < size - 1; startPos++){
+    //         if (diff > 0 && nums[startPos + 1] - nums[startPos] < 0) {return false;}
+    //         else if (diff < 0 && nums[startPos + 1] - nums[startPos] > 0) {return false;}
+    //     }
+    //     return true;
+    // }
 };
