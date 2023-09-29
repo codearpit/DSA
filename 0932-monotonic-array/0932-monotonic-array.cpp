64fc0b68cc7a02938c95bbb402e9b86a36a1a537
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-09-29 19:34:53 +0530

    Time: 129 ms (35.19%), Space: 96.7 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     bool isMonotonic(vector<int>& arr) {
         int i = 0;
-        while(i<arr.size() - 1){
+        int n = arr.size();
+        while(i<n - 1){
             if(arr[i] == arr[i+1]) i++;
 
             else if(arr[i] < arr[i+1]){
-                while(i < arr.size() - 1){
+                while(i < n - 1){
                     if(arr[i] > arr[i+1]) return false;
                     i++;
                 }
             }
             else{
-             while(i < arr.size() - 1){
+             while(i < n - 1){
                     if(arr[i] < arr[i+1]) return false;
                     i++;
                 }
