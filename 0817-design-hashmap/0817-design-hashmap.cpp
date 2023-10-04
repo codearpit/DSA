901232109f6b3e4c4717fe675a075eb9fdcbfba2
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-10-04 16:46:11 +0530

    Time: 101 ms (82.5%), Space: 61.2 MB (52.39%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,21 +1,54 @@
 class MyHashMap {
+	vector<vector<pair<int, int>>> map;
+	const int size = 10000;
 public:
-    int data[1000001];
-    MyHashMap() {
-        fill(data,data + 1000000,-1);
-    }
-    
-    void put(int key, int value) {
-        data[key] = value;
-    }
-    
-    int get(int key) {
-        return data[key];
-    }
-    
-    void remove(int key) {
-        data[key] = -1;
-    }
+	/** Initialize your data structure here. */
+	MyHashMap() {
+		map.resize(size);
+	}
+
+	/** value will always be non-negative. */
+	void put(int key, int value) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+        for(auto iter = row.begin(); iter != row.end(); iter++)
+        {
+            if(iter->first == key)
+            {
+                iter->second = value;
+                return;
+            }
+        }
+		row.push_back(make_pair(key, value));
+	}
+
+	/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+	int get(int key) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+		for (auto iter = row.begin(); iter != row.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				return iter->second;
+			}
+		}
+		return -1;
+	}
+
+	/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+	void remove(int key) {
+		int index = key % size;
+        vector<pair<int, int>> &row = map[index];
+		for (auto iter = row.begin(); iter != row.end(); iter++)
+		{
+			if (iter->first == key)
+			{
+				row.erase(iter);
+                return;
+			}
+		}
+	}
 };
 
 /**
