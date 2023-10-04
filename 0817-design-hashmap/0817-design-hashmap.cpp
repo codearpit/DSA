e0365313ac9c732119985c0b2dac07146c994f24
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-10-04 18:38:31 +0530

    Time: 310 ms (6.66%), Space: 50.8 MB (96.52%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,54 +1,46 @@
 class MyHashMap {
-	vector<vector<pair<int, int>>> map;
-	const int size = 1000;
 public:
-	/** Initialize your data structure here. */
-	MyHashMap() {
-		map.resize(size);
-	}
-
-	/** value will always be non-negative. */
-	void put(int key, int value) {
-		int index = key % size;
-        vector<pair<int, int>> &row = map[index];
-        for(auto iter = row.begin(); iter != row.end(); iter++)
-        {
-            if(iter->first == key)
-            {
-                iter->second = value;
-                return;
+    vector<pair<int, int>> hashMap;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        if(hashMap.size() == 0){
+            hashMap.push_back({key, value});
+        }
+        else{
+            bool present = false;
+            for(int i=0;i<hashMap.size();i++){
+                if(hashMap[i].first == key){
+                    present = true;
+                    hashMap[i].second = value;
+                    break;
+                }
+            }
+            if(!present){
+                hashMap.push_back({key, value});
             }
         }
-		row.push_back(make_pair(key, value));
-	}
-
-	/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-	int get(int key) {
-		int index = key % size;
-        vector<pair<int, int>> &row = map[index];
-		for (auto iter = row.begin(); iter != row.end(); iter++)
-		{
-			if (iter->first == key)
-			{
-				return iter->second;
-			}
-		}
-		return -1;
-	}
-
-	/** Removes the mapping of the specified value key if this map contains a mapping for the key */
-	void remove(int key) {
-		int index = key % size;
-        vector<pair<int, int>> &row = map[index];
-		for (auto iter = row.begin(); iter != row.end(); iter++)
-		{
-			if (iter->first == key)
-			{
-				row.erase(iter);
-                return;
-			}
-		}
-	}
+    }
+    
+    int get(int key) {
+        for(int i=0;i<hashMap.size();i++){
+            if(hashMap[i].first == key){
+                return hashMap[i].second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        for(int i = 0; i < hashMap.size(); ++i){
+            if(hashMap[i].first == key){
+                hashMap.erase(hashMap.begin() + i);
+                break;
+            }
+        }
+    }
 };
 
 /**
