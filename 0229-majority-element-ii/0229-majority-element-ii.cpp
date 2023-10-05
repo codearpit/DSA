5386ef0262166c2e4854dfd1c516b0b4028fe23f
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-10-05 18:28:31 +0530

    Time: 6 ms (90%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int countMajority = nums.size()/3;
-        unordered_map<int, int> map;
-        vector<int> output;
-        for(auto num : nums){
-            map[num]++;
+        if(nums.size() == 1){
+            return {nums[0]};
         }
-        for(auto num : map){
-            if(num.second > countMajority){
-                output.push_back(num.first);
+        if(nums.size() == 2 && nums[0] != nums[1]){
+            return {nums[0], nums[1]};
+        }
+        sort(nums.begin(), nums.end());
+        int countMajority = nums.size()/3;
+        unordered_set<int> set;
+        vector<int> output;
+        int count = 1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i-1] == nums[i]){
+                count++;
+                if(count > countMajority){
+                    set.insert(nums[i]);
+                }
             }
+            else{
+                count = 1;
+            }
+        }
+        for(int it : set){
+            output.push_back(it);
         }
         return output;
     }
