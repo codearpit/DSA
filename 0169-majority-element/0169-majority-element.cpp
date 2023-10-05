dba6f02c5da429fe69dda882753aa45898d12ec3
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-10-05 17:54:59 +0530

    Time: 15 ms (51.17%), Space: 19.9 MB (18%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,5 +1,24 @@
 // https://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = 0;
+        for (unsigned int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+            int bits = 0;
+            for (int num : nums) {
+                if (num & mask) {
+                    bits++;
+                }
+            }
+            if (bits > nums.size() / 2) {
+                majority |= mask;
+            }
+        }
+        return majority;
+    }
+};
 
+/*
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -25,4 +44,4 @@ public:
     
     return element;
     }
-};
+};*/
