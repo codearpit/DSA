6dc6221fae585d44a82b51cbccfe607d63cf3318
Arpit Bagri <32553155+codearpit@users.noreply.github.com>
2023-10-11 00:18:52 +0530

    Time: 151 ms (73.01%), Space: 64.5 MB (71.55%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int ans = n;
-        
-        // std::set automatically sorts
-        set<int> unique(nums.begin(), nums.end());
-        vector<int> newNums;
-        for (int num : unique) {
-            newNums.push_back(num);
-        }
-        
-        int j = 0;
-        for (int i = 0; i < newNums.size(); i++) {
-            while (j < newNums.size() && newNums[j] < newNums[i] + n) {
-                j++;
+        // Sort the vector in ascending order
+        sort(nums.begin(), nums.end());
+
+        // Initialize variables
+        int n = nums.size(); // Number of elements in the vector
+        int left = 0; // Left pointer for the sliding window
+        int maxCount = 1; // Initialize the maximum count of distinct elements
+        int currentCount = 1; // Initialize the count of distinct elements in the current window
+
+        // Iterate through the vector to find the minimum operations
+        for (int right = 1; right < n; ++right) {
+            // Check if the current element is equal to the previous one
+            if (nums[right] == nums[right - 1]) {
+                continue; // Skip duplicates
             }
-            
-            int count = j - i;
-            ans = min(ans, n - count);
+
+            // Check if the current window size is less than or equal to the difference between the maximum and minimum values
+            while (nums[right] - nums[left] > n - 1) {
+                // Move the left pointer to shrink the window
+                if(left<n && nums[left+1]==nums[left]){
+currentCount++;
+}
+                left++;
+                currentCount--;
+            }
+
+            // Update the count of distinct elements in the current window
+            currentCount++;
+
+            // Update the maximum count
+            maxCount = max(maxCount, currentCount);
         }
-        
-        return ans;
+
+        // Calculate the minimum operations
+        int minOps = n - maxCount;
+
+        return minOps;
     }
 };
